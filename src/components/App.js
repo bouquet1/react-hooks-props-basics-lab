7f00e83e98ec878f08bad4cfb1cb8d5424973cc8
bouquet1 <bukuleta@gmail.com>
2023-05-26 18:37:10 -0700

    still errors

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links"
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
@@ -11,8 +12,18 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        city="New York" 
+        name="Liza" 
+        color="firebrick" 
+      />
+      <About 
+        bio = "I made this" 
+      />
+      <Links 
+        github= "https://github.com/liza"
+        linkedin= "" 
+      />
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Links(props) {
+    return (
+        <div>
+        <h3>Links</h3>
+        <a href={props.github} alt="liza's github link" />
+        </div>
+    )
+}
+
+export default Links;
